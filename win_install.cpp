c6748605b43ed66e55f057bc8a88df52e327507e
oxygen <howard2805@gmail.com>
2017-08-13 19:32:35 +0800

    Add installer

---
+++ win_install.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <cstdio>
+int main()
+{
+    system("mkdir \"C:\\Program Files\\Gene-Translation\"");
+    //Copy files
+    system("copy Gene-run.exe \"C:\\Program Files\\Gene-Translation\"");
+
+    system("copy transcripter.py \"C:\\Program Files\\Gene-Translation\"");
+    system("copy translater.py \"C:\\Program Files\\Gene-Translation\"");
+    //Set EV
+    system("set Gene-run=C:\\Program Files\\Gene-Translation\\Gene-run.exe");
+    //printf("copy Gene-run.exe  \"C:\\Program Files\\Gene-Translation\"");
+    system("pause");
+    return 0;
+}
